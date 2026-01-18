240d1d9c9bfd6f2b30239aa888ed5649e7cd51b0
Praveen kumar <pk7476807@gmail.com>
2026-01-18 23:01:50 +0530

    initial django project

---
+++ myproject/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.http import FileResponse
+from django.conf import settings
+import os
+
+def service_worker(request):
+    return FileResponse(
+        open(os.path.join(settings.BASE_DIR, "sw.js"), "rb"),
+        content_type="application/javascript"
+    )
+
+def manifest(request):
+    return FileResponse(
+        open(os.path.join(settings.BASE_DIR, "water/static/water/manifest.json"), "rb"),
+        content_type="application/json"
+    )
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("water.urls")),
+    path("sw.js", service_worker),          # ✅ ROOT
+    path("manifest.json", manifest),        # ✅ ROOT
+]
+
---
+++ water/static/water/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "hydration-cache-v1";
+const urlsToCache = [
+  "/remainder/",
+  "/static/water/style.css",
+  "/static/water/watrer_caterpillar.avif",
+  "/static/water/icon-256.png",
+  "/static/water/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
