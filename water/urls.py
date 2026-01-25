1ee93a366356ee42c90f454fc34daaa222f9086f
Praveen kumar <pk7476807@gmail.com>
2026-01-25 20:17:24 +0530

    Add homepage / fix 404

--- water/urls.py
+++ water/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from .import views
 
-urlpatterns = [ path('remainder/',views.remainder,name='remainder'),]
+urlpatterns = [ 
+        path('', views.home, name='home'), 
+        path('remainder/',views.remainder,name='remainder'),]
